abbdbac3c1691f2cdc5d88befa8317a776c67953
Antony Pavlov <antonynpavlov@gmail.com>
2015-11-14 08:35:25 +0300

    move common SoC code to the separate file
    Signed-off-by: Antony Pavlov <antonynpavlov@gmail.com>

--- Hardware/boards/de0-nano/de0-nano.v
+++ Hardware/boards/de0-nano/de0-nano.v
@@ -16,75 +16,6 @@ module reset(clk, reset);
 			reset <= 0;
 endmodule /* reset */
 
-module soc(
-	input  clock_50MHz,
-	input  reset,
-	output [31:0] wb_iadr_o,
-	input  uart_rx,
-	output uart_tx
-	);
-
-	//wire reset;
-	//assign reset = !reset_n;
-
-	wire wb_clk;
-	assign wb_clk = clock_50MHz;
-	wire wb_rst;
-	assign wb_rst = reset;
-
-`include "wb_intercon.vh"
-
-	rom bootrom(
-		.wb_clk(wb_clk),
-		.wb_rst(wb_rst),
-
-		.wb_adr_i(wb_m2s_rom0_adr[10:2]),
-		.wb_stb_i(wb_m2s_rom0_stb),
-		.wb_cyc_i(wb_m2s_rom0_cyc),
-		.wb_dat_o(wb_s2m_rom0_dat),
-		.wb_ack_o(wb_s2m_rom0_ack)
-	);
-
-	uart_top uart16550(
-		.wb_clk_i(wb_clk),
-		.wb_rst_i(wb_rst),
-
-		.wb_adr_i(wb_m2s_uart0_adr[4:0]),
-		.wb_dat_i(wb_m2s_uart0_dat),
-		.wb_sel_i(wb_m2s_uart0_sel),
-		.wb_we_i(wb_m2s_uart0_we),
-		.wb_cyc_i(wb_m2s_uart0_cyc),
-		.wb_stb_i(wb_m2s_uart0_stb),
-		.wb_dat_o(wb_s2m_uart0_dat),
-		.wb_ack_o(wb_s2m_uart0_ack),
-
-		.stx_pad_o(uart_tx),
-		.srx_pad_i(uart_rx)
-	);
-
-	mips32r1_wb mips32r1_wb(
-		.wb_clk_i(clock_50MHz),
-		.wb_rst_i(reset),
-
-		.iwbm_adr_o(wb_m2s_mips32r1_i_adr),
-		.iwbm_dat_i(wb_s2m_mips32r1_i_dat),
-		.iwbm_stb_o(wb_m2s_mips32r1_i_stb),
-		.iwbm_ack_i(wb_s2m_mips32r1_i_ack),
-		.iwbm_cyc_o(wb_m2s_mips32r1_i_cyc),
-
-		.dwbm_adr_o(wb_m2s_mips32r1_d_adr),
-		.dwbm_dat_i(wb_s2m_mips32r1_d_dat),
-		.dwbm_stb_o(wb_m2s_mips32r1_d_stb),
-		.dwbm_ack_i(wb_s2m_mips32r1_d_ack),
-		.dwbm_cyc_o(wb_m2s_mips32r1_d_cyc),
-		.dwbm_dat_o(wb_m2s_mips32r1_d_dat),
-		.dwbm_we_o(wb_m2s_mips32r1_d_we),
-		.dwbm_sel_o(wb_m2s_mips32r1_d_sel)
-	);
-
-	assign wb_iadr_o = wb_m2s_mips32r1_i_adr;
-endmodule
-
 module de0_nano(
 	input  CLOCK_50,
 	input  [3:0]  SW,
@@ -119,8 +50,8 @@ module de0_nano(
 
 	wire [31:0] wb_iadr_o;
 
-	soc soc(
-		.clock_50MHz(clk10m),
+	mips32r1_soc soc(
+		.clock(clk10m),
 		.reset(my_reset),
 		.wb_iadr_o(wb_iadr_o),
 		.uart_rx(GPIO_0[32]),
---
+++ Hardware/src/mips32r1_soc.v
@@ -0,0 +1,65 @@
+module mips32r1_soc(
+	input  clock,
+	input  reset,
+	output [31:0] wb_iadr_o,
+	input  uart_rx,
+	output uart_tx
+	);
+
+	wire wb_clk;
+	assign wb_clk = clock;
+	wire wb_rst;
+	assign wb_rst = reset;
+
+`include "wb_intercon.vh"
+
+	rom bootrom(
+		.wb_clk(wb_clk),
+		.wb_rst(wb_rst),
+
+		.wb_adr_i(wb_m2s_rom0_adr[10:2]),
+		.wb_stb_i(wb_m2s_rom0_stb),
+		.wb_cyc_i(wb_m2s_rom0_cyc),
+		.wb_dat_o(wb_s2m_rom0_dat),
+		.wb_ack_o(wb_s2m_rom0_ack)
+	);
+
+	uart_top uart16550(
+		.wb_clk_i(wb_clk),
+		.wb_rst_i(wb_rst),
+
+		.wb_adr_i(wb_m2s_uart0_adr[4:0]),
+		.wb_dat_i(wb_m2s_uart0_dat),
+		.wb_sel_i(wb_m2s_uart0_sel),
+		.wb_we_i(wb_m2s_uart0_we),
+		.wb_cyc_i(wb_m2s_uart0_cyc),
+		.wb_stb_i(wb_m2s_uart0_stb),
+		.wb_dat_o(wb_s2m_uart0_dat),
+		.wb_ack_o(wb_s2m_uart0_ack),
+
+		.stx_pad_o(uart_tx),
+		.srx_pad_i(uart_rx)
+	);
+
+	mips32r1_wb mips32r1_wb(
+		.wb_clk_i(wb_clk),
+		.wb_rst_i(wb_rst),
+
+		.iwbm_adr_o(wb_m2s_mips32r1_i_adr),
+		.iwbm_dat_i(wb_s2m_mips32r1_i_dat),
+		.iwbm_stb_o(wb_m2s_mips32r1_i_stb),
+		.iwbm_ack_i(wb_s2m_mips32r1_i_ack),
+		.iwbm_cyc_o(wb_m2s_mips32r1_i_cyc),
+
+		.dwbm_adr_o(wb_m2s_mips32r1_d_adr),
+		.dwbm_dat_i(wb_s2m_mips32r1_d_dat),
+		.dwbm_stb_o(wb_m2s_mips32r1_d_stb),
+		.dwbm_ack_i(wb_s2m_mips32r1_d_ack),
+		.dwbm_cyc_o(wb_m2s_mips32r1_d_cyc),
+		.dwbm_dat_o(wb_m2s_mips32r1_d_dat),
+		.dwbm_we_o(wb_m2s_mips32r1_d_we),
+		.dwbm_sel_o(wb_m2s_mips32r1_d_sel)
+	);
+
+	assign wb_iadr_o = wb_m2s_mips32r1_i_adr;
+endmodule
