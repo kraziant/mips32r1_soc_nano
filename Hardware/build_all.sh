f8e093aceaa596544b1a32b732bc5f6f60ed70e8
Antony Pavlov <antonynpavlov@gmail.com>
2015-11-14 13:09:51 +0300

    add build_all.sh script
    Signed-off-by: Antony Pavlov <antonynpavlov@gmail.com>

---
+++ Hardware/build_all.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+
+report_board()
+{
+	PROJ=$1
+	NUM_COLUMNS=$2
+
+	echo $PROJ
+	echo
+
+	grep -B 1 -A $NUM_COLUMNS "; Flow Summary" $PROJ/*.flow.rpt
+	grep -B 1 -A 5 "; Slow .* 85C Model Fmax Summary" $PROJ/*.sta.rpt | sed "s/^.*.sta.rpt.//"
+
+	echo
+}
+
+
+SAVE_PATH=$PATH
+set -e
+
+export PATH=$SAVE_PATH:/opt/altera/13.1/quartus/bin
+QUARTUS_NAME=q13.1
+
+for BOARD in de0-nano de1-soc marsohod2; do
+	PROJ="proj-$QUARTUS_NAME-$BOARD"
+
+	./make_project.sh $BOARD $PROJ
+	cd $PROJ
+	# We have to use 64-bit Quartus for Cyclone V
+	make QFLAGS="--64bit" mips32r1-soc.svf
+	cd ..
+
+	cp $PROJ/mips32r1-soc.sof mips32r1-$QUARTUS_NAME-$BOARD.sof
+	cp $PROJ/mips32r1-soc.svf mips32r1-$QUARTUS_NAME-$BOARD.svf
+done
+
+REPORT=REPORT.$QUARTUS_NAME
+echo > $REPORT
+report_board "proj-$QUARTUS_NAME-de0-nano" 19 >> $REPORT
+report_board "proj-$QUARTUS_NAME-de1-soc" 22 >> $REPORT
+report_board "proj-$QUARTUS_NAME-marsohod2" 19 >> $REPORT
+
+export PATH=$SAVE_PATH:/opt/altera/15.0/quartus/bin
+QUARTUS_NAME=q15.0
+
+for BOARD in de0-nano de1-soc marsohod3; do
+	PROJ="proj-$QUARTUS_NAME-$BOARD"
+
+	./make_project.sh $BOARD $PROJ
+	cd $PROJ
+	make mips32r1-soc.svf
+	cd ..
+
+	cp $PROJ/mips32r1-soc.sof mips32r1-$QUARTUS_NAME-$BOARD.sof
+	cp $PROJ/mips32r1-soc.svf mips32r1-$QUARTUS_NAME-$BOARD.svf
+done
+
+REPORT=REPORT.$QUARTUS_NAME
+echo > $REPORT
+report_board "proj-$QUARTUS_NAME-de0-nano" 19 >> $REPORT
+report_board "proj-$QUARTUS_NAME-de1-soc" 22 >> $REPORT
+report_board "proj-$QUARTUS_NAME-marsohod3" 21 >> $REPORT
